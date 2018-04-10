a1e431a51eb3fdc9e8dd40b565ea1fbc9f2f8ec2
ly2011 <1518550424@qq.com>
2018-04-10 23:47:19 +0800

    修改store/reducer/home

--- src/store/reducers/home.js
+++ src/store/reducers/home.js
@@ -1,9 +1,17 @@
 import * as types from '@/store/types';
 
+// newList: [
+//   {id: 1, list:[], title: '推荐'},
+//   {id: 2, list: [], title: '视频'},
+//   {id: 3, list: [], title: '热点'}
+// ]
 const initialState = {
   topics: [],
-  newsList: [],
-  newsIndex: 0
+  newsList: [], // 首页全部数据存储
+  newsIndex: 0, // 当前的tab栏 0-推荐，1-视频，2-热点...
+  newsPrevIndex: 0,
+  newsLoading: false,
+  end: false // 是否已经加载完毕
 };
 
 const homeState = (state = initialState, action) => {
